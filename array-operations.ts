02f6f678d4e215cf1f2e1c577a28d024e24bd570
ericmadu <eric.madureira@nextroll.com>
2021-06-08 11:13:15 -0300

    unique function

--- array-operations.ts
+++ array-operations.ts
@@ -2,3 +2,7 @@
 export const empty = (providedArray: any[]) => {
     return Array.isArray(providedArray) && providedArray.length;
 };
+
+export const unique = (providedArray: any[]) => {
+    return [...new Set(providedArray)];
+};
