e9c556abd15f0b96a5b90d4b593bd09cf79c3106
Sebastian Lozano <sebastian.lozano@nextroll.com>
2021-06-09 17:25:30 -0500

    Add functions used in example
    Add needed configuration line for test purposes

--- array-operations.ts
+++ array-operations.ts
@@ -4,10 +4,64 @@ export const empty = (providedArray: any[]) => {
 };
 
 export const groupBy = (providedArray: any[], key: string) => {
-    return providedArray.reduce((result: Object, element: any) => {
+    return providedArray.reduce((result: any, element: any) => {
         (result[element[key]] = result[element[key]] || []).push(element);
         return result;
     }, {});
+};
+
+export const negate = function (predicate: Function) {
+    if (typeof predicate !== 'function') {
+        throw new Error("expected a function")
+    }
+    return function (...args: any) {
+        return !predicate.apply(this, args);
+    }
+}
+
+export const countBy = (providedArray: any[], key: string) => {
+    const accumulator: any = {};
+    providedArray.forEach((currentValue) => {
+        accumulator[currentValue[key]] = (accumulator[currentValue[key]] || 0) + 1;
+    });
+    return accumulator;
+};
+
+export const sortBy = (providedArray: any[], key: string) => {
+    return providedArray.sort((a, b) => (a[key] > b[key]) ? 1 : ((b[key] > a[key]) ? -1 : 0));
+};
+
+export const filter = (providedArray: any[], predicate: Function) => {
+    let index = -1;
+    let resIndex = 0;
+    const length = providedArray == null ? 0 : providedArray.length;
+    const result = [];
+
+    while (++index < length) {
+        const value = providedArray[index];
+        if (predicate(value, index, providedArray)) {
+            result[resIndex++] = value;
+        }
+    }
+    return result;
+}
+
+export const filterObject = (object: any, predicate: Function) => {
+    object = Object(object);
+    const result: any[] = [];
+
+    Object.keys(object).forEach((key) => {
+        const value = object[key];
+        if (predicate(value, key, object)) {
+            result.push(value);
+        }
+    });
+    return result;
+};
+
+export const reject = (providedArray: any[], predicate: Function) => {
+    const func = Array.isArray(providedArray) ? filter : filterObject;
+    return func(providedArray, negate(predicate));
 };
 
 export const unique = (providedArray: any[]) => {
